99d8a9fe5b56523d4996200cb756a1df33140cc6
wangjiejia <1481287087@qq.com>
2021-04-30 17:33:04 +0800

    4月30----运营后台修改账户资料

---
+++ tests/test_coupon_create.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/4/30 17:30
+@Auth ： jiejia
+@File ：coupon.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+"""
+"""生成F码"""
+import requests
+from tools.gettoken import test_headers
+headers=test_headers()
+def test_coupon_create():
