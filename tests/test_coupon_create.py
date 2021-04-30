174457fb15f585f3a2076b38f7abcdf34dea7312
wangjiejia <1481287087@qq.com>
2021-04-30 17:42:39 +0800

    4月30----运营后台修改账户资料

--- tests/test_coupon_create.py
+++ tests/test_coupon_create.py
@@ -10,4 +10,32 @@
 import requests
 from tools.gettoken import test_headers
 headers=test_headers()
-def test_coupon_create():
+"""F码名称未填写"""
+url="https://live-admin-qa1.youfenba.com/api/v1/coupon"
+# def test_coupon_create_nameisnull():
+#     dict={
+#         "channel_id":98,
+#         "channel_remark": "测试测试测试测试测手册",
+#         "count": 1,
+#         "name": "",
+#         "type": 1,
+#         "type_discount": 11100,
+#         "use_condition": 100
+#     }
+#     res = requests.post(url=url,data=dict,headers=headers)
+#     r=res.json()
+#     assert r['message'] == "请输入优惠券名称"
+
+def test_coupon_create_discountisnull():
+    dict={
+        "channel_id":98,
+        "channel_remark": "测试测试测试测试测手册",
+        "count": 1,
+        "name": "接口测试",
+        "type": 1,
+        "type_discount": "",
+        "use_condition": 100
+    }
+    res = requests.post(url=url,data=dict,headers=headers)
+    r=res.json()
+    assert r['message'] == "请输入优惠券金额"
