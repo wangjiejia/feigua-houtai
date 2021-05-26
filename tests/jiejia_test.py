ad72de70de9afd58e5a8d10cca846e9993565426
wangjiejia <1481287087@qq.com>
2021-05-26 13:39:52 +0800

    5月25--拉取执行分支

---
+++ tests/jiejia_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/5/25 17:13
+@Auth ： jiejia
+@File ：jiejia_test.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+"""
+"""测试测试"""
+import pytest
+def test_jiejie():
+    print("验证")
+
