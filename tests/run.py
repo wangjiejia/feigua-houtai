f4e29acf888f791c319f3bfb1f1a28a20eb1f3e2
wangjiejia <1481287087@qq.com>
2021-05-26 11:12:59 +0800

    5月25--拉取执行分支

--- tests/run.py
+++ tests/run.py
@@ -19,7 +19,7 @@ import os
 import allure
 if __name__=='__main__':
     """此时生成的报告文件为json格式"""
-    pytest.main(["-s","--alluredir","./report","test_user_create.py"])
+    pytest.main(["-s","--alluredir","./report","C:/Users/1/PycharmProjects/feigua-houtai/tests/jiejia_test.py"])
     """将json格式转换成html格式"""
     os.system('allure generate ./report/ -o ./report/html/ --clean')
     """右键使用浏览器打开正确，直接复制出去打开失败；可先通过本地渲染，再进行打开"""
