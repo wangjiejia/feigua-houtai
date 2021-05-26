2f46b86537dfe482cbbe9241672a8d50360ca96d
wangjiejia <1481287087@qq.com>
2021-05-26 13:40:01 +0800

    5月25--拉取执行分支

--- run.py
+++ run.py
@@ -10,15 +10,20 @@
 import pytest
 import os
 import allure
+
+# if __name__=='__main__':
+#     """此时生成的报告文件为json格式"""
+#     # pytest.main(["-s", "--alluredir", "生成报告路径", "执行的测试文件"])
+#     # pytest.main(["-s","alluredir","./report","gettoken.py"])
+#     # # pytest.main(["-s","--alluredir","./report","user_test.py"])
+#     # """将json格式转换成html格式"""
+#     # os.system('allure generate ./report/ -o ./report/html/ --clean')
+#     # """右键使用浏览器打开正确，直接复制出去打开失败；可先通过本地渲染，再进行打开"""
+#     os.system("allure open ./report/html/")
+#     pytest.main(['-s', '-q', r'C:\Users\1\PycharmProjects\feigua-houtai\tools\gettoken.py', '--alluredir',
+#                  './report/xml'])
 if __name__=='__main__':
-    """此时生成的报告文件为json格式"""
-    # pytest.main(["-s", "--alluredir", "生成报告路径", "执行的测试文件"])
-    pytest.main(["-s","alluredir","./report","gettoken.py"])
-    # pytest.main(["-s","--alluredir","./report","user_test.py"])
-    """将json格式转换成html格式"""
-    os.system('allure generate ./report/ -o ./report/html/ --clean')
-    """右键使用浏览器打开正确，直接复制出去打开失败；可先通过本地渲染，再进行打开"""
-    os.system("allure open ./report/html/")
+    pytest.main(["-s", "--alluredir", "C:/Users/1/PycharmProjects/feigua-houtai/report", "gettoken.py"])
 
 
 
